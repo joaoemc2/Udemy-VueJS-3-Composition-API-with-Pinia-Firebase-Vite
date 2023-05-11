d84a5d72d415fb0b0f257f19f54af20cbd644b79
João Vitor Guimarães <92169565+joaoemc2@users.noreply.github.com>
2023-05-11 17:37:02 -0300

    first commit

---
+++ vue-composition-api-course/src/views/HomeView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+    <h2>{{ appTitle }}</h2>
+
+    <h3>{{ counterData.title }}:</h3>
+
+    <div>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
+    </div>
+
+    <p>This count is {{ oddOrEven }}</p>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input type="text" v-model="counterData.title" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, computed } from "vue";
+
+const appTitle = "Hello World";
+
+const counterData = reactive({
+  count: 0,
+  title: "My Counter",
+});
+
+const oddOrEven = computed(() => {
+  if (counterData.count % 2 == 0) return "even";
+  else return "odd";
+});
+
+const increaseCounter = (amount, e) => {
+  console.log(e);
+  counterData.count += amount;
+};
+const decreaseCounter = (amount) => {
+  counterData.count -= amount;
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+.edit {
+  margin-top: 32px;
+}
+</style>
